cacb3c8aa0b7d126e7d15a1d6a9838087bc8c043
Tianyi Zhang <tianyi.zhang6546@gmail.com>
2022-04-22 14:00:57 -0400

    Implemented 01-data_cleaning.R.

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,100 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the Dataset
+# Author: Tian Yi Zhang
+# Date: 22 April 2022
+# Contact: psyminette.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
-library(haven)
 library(tidyverse)
+library(dplyr)
+library(janitor)
+library(pointblank)
+
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
-
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+# Dataset was downloaded from survey in Google Forms. Columns names are
+# already cleaned.
+# Additional Cleaning done beforehand:
+# - Total spending from packages is calculated based on packages
+# purchased using excel
+# - New column of 0 and 1; 1 if got the game for free
+raw_data <- readr::read_csv("inputs/data/raw_data.csv")
          
+#### Data Cleaning ####
+# Remove malicious responses intended to sabotage the survey
+# Row 45, 52, and 131 are obvious malicious responses
+cleaned_data <- raw_data[-c(45,52,131),]
 
-#### What's next? ####
+#### Dataset Validation ####
+# Change the class of the variables so that they are considered factors when
+# appropriate
+cleaned_data <- cleaned_data |>
+  mutate(age = as.numeric(age)) |>
+  mutate(gender = as.factor(gender)) |>
+  mutate(character_total = as.factor(character_total)) |>
+  mutate(character_female = as.factor(character_female)) |>
+  mutate(playtime_total = as.numeric(playtime_total)) |>
+  mutate(playtime_main = as.numeric(playtime_main)) |>
+  mutate(gender_main = as.factor(gender_main)) |>
+  mutate(packages_usd = as.numeric(packages_usd)) |>
+  mutate(package_free = as.factor(package_free)) |>
+  mutate(importance_vp = as.numeric(importance_vp)) |>
+  mutate(importance_bkom = as.numeric(importance_bkom)) |>
+  mutate(cm_vp_count = as.numeric(cm_vp_count)) |>
+  mutate(cm_bkom_count = as.numeric(cm_bkom_count)) |>
+  mutate(ps_vp_usd = as.numeric(ps_vp_usd)) |>
+  mutate(ps_bkom_usd = as.numeric(ps_bkom_usd)) |>
+  mutate(cm_outfits = as.numeric(cm_outfits)) |>
+  mutate(ps_cosmetic_usd = as.numeric(ps_cosmetic_usd)) |>
+  mutate(ps_functional_usd = as.numeric(ps_functional_usd)) |>
+  mutate(ps_total = as.numeric(ps_total))
 
+# Create test agent
+agent <-
+  create_agent(tbl = cleaned_data) |>
+  col_is_factor(columns = vars(gender,
+                               character_total,
+                               character_female,
+                               gender_main,
+                               package_free
+                               )) |>
+  col_is_numeric(columns = vars(age,
+                                playtime_total,
+                                playtime_main,
+                                packages_usd,
+                                importance_vp,
+                                importance_bkom,
+                                cm_vp_count,
+                                cm_bkom_count,
+                                ps_vp_usd,
+                                ps_bkom_usd,
+                                cm_outfits,
+                                ps_cosmetic_usd,
+                                ps_functional_usd
+                                )) |>
+  col_vals_in_set(columns = gender,
+                  set = c("Male", "Female","Other")) |>
+  col_vals_in_set(column = gender_main,
+                  set = c("Male", "Female")) |>
+  col_vals_gte(columns = c(age,
+                           playtime_total,
+                           playtime_main,
+                           packages_usd,
+                           importance_vp,
+                           importance_bkom,
+                           cm_vp_count,
+                           cm_bkom_count,
+                           ps_vp_usd,
+                           ps_bkom_usd,
+                           cm_outfits,
+                           ps_cosmetic_usd,
+                           ps_functional_usd
+                           ),
+               value = 0) %>%
+  interrogate()
 
+agent
 
-         
+#### Save data ####
+write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
